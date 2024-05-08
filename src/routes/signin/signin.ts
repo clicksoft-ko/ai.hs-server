8bee0d1f63f41b3ead1a47ea62b2938ad143645d
Kaburi <specialemail90@gmail.com>
2024-05-08 14:38:55 +0900

    .

--- src/routes/signin/signin.ts
+++ src/routes/signin/signin.ts
@@ -1,4 +1,10 @@
-import { Router, Request, Response, NextFunction } from "express";
+import {
+  Router,
+  Request,
+  Response,
+  NextFunction,
+  CookieOptions,
+} from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../../models/user";
 import { BadRequestError } from "../../errors/bad-request-error";
@@ -25,16 +31,23 @@ router.post(
       throw new BadRequestError(errorMessage, "_form");
     }
 
-    const expiresInMinutes = 60;
-    const userJwt = jwt.sign({ userId: user.userId, roomKey: user.roomKey }, process.env.JWT_KEY!, {
-      expiresIn: expiresInMinutes * 60,
-    });
-    // req.session = { jwt: userJwt };
-    res.cookie("jwt", userJwt, {
+    const expiresInMinutes = 60 * 24 * 365;
+    const userJwt = jwt.sign(
+      { userId: user.userId, roomKey: user.roomKey },
+      process.env.JWT_KEY!,
+      {
+        expiresIn: expiresInMinutes * 60,
+      }
+    );
+
+    const cookieOptions: CookieOptions = {
       maxAge: expiresInMinutes * 60 * 1000,
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-    });
+      secure: process.env.NODE_ENV === "production",
+    };
+
+    res.cookie("user", JSON.stringify(user), cookieOptions);
+    res.cookie("jwt", userJwt, cookieOptions);
     res.status(201).send(user);
   }
 );
--- src/routes/signout/signout.ts
+++ src/routes/signout/signout.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
+  res.cookie("user", "", { maxAge: 0, httpOnly: true });
   res.cookie("jwt", "", { maxAge: 0, httpOnly: true });
 
   res.send({});
