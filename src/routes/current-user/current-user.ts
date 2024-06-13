a3017e03e7bd210fde985bfabef02f3ea0d744be
Kaburi <specialemail90@gmail.com>
2024-06-13 15:49:27 +0900

    docker hub 업로드 추가

--- src/routes/current-user/current-user.ts
+++ src/routes/current-user/current-user.ts
@@ -1,8 +1,19 @@
 import { Router, Request, Response } from "express";
 import { currentUser } from "../../middlewares/current-user";
 import { requireAuth } from "../../middlewares/require-auth";
+import { URLConst } from "@/constants/url-const";
 
 const router = Router();
+
+router.get("/",
+  async (req: Request, res: Response) => {
+    const data = {
+      urlconstClient: URLConst.CLIENT,
+      nodeEnv: process.env.NODE_ENV,
+      baseUrl: process.env.BASE_URL,
+    }
+    res.send(data);
+  })
 
 router.post(
   "/",
