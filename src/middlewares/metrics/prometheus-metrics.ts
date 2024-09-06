0d915346233a6a1d694bdf0ead3792fce224b158
Kaburi <specialemail90@gmail.com>
2024-09-06 10:11:48 +0900

    .

--- src/middlewares/metrics/prometheus-metrics.ts
+++ src/middlewares/metrics/prometheus-metrics.ts
@@ -12,7 +12,7 @@ class PrometheusMetrics {
   #requestSuccessHistogram = new Histogram({
     name: 'express_success_requests',
     help: 'Express success requests - duration in seconds',
-    labelNames: ['handler', 'controller', 'method'],
+    labelNames: ['path', 'method'],
     buckets: [
       0.0001, 0.001, 0.005, 0.01, 0.025, 0.05, 0.075, 0.09, 0.1, 0.25, 0.5, 1,
       2.5, 5, 10,
@@ -23,7 +23,7 @@ class PrometheusMetrics {
   #requestFailHistogram = new Histogram({
     name: 'express_fail_requests',
     help: 'Express fail requests - duration in seconds',
-    labelNames: ['handler', 'controller', 'method'],
+    labelNames: ['path', 'method'],
     buckets: [
       0.0001, 0.001, 0.005, 0.01, 0.025, 0.05, 0.075, 0.09, 0.1, 0.25, 0.5, 1,
       2.5, 5, 10,
@@ -33,7 +33,7 @@ class PrometheusMetrics {
   #failureCounter = new Counter({
     name: 'express_requests_failed_count',
     help: 'Express requests that failed',
-    labelNames: ['handler', 'controller', 'error', 'method'],
+    labelNames: ['path', 'error', 'method'],
   });
 
   #isAvailableMetricsUrl(url: string): boolean {
