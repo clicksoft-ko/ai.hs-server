90f6895465c4bb70b34f881609ba142a03a70633
Kaburi <specialemail90@gmail.com>
2024-09-27 16:54:45 +0900

    .

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -15,10 +15,12 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db?.collections();
 
-  for (let collection of collections) {
-    await collection.deleteMany({});
+  if (collections) {
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
   }
 });
 
